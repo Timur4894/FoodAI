e058d8208278ef956ac3e2f63d64d69dc42f819a
Tima <bcob298@gmail.com>
2023-10-10 21:17:08 +0200

    Initial Commit

---
+++ foodAI/foodAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodAIApp.swift
+//  foodAI
+//
+//  Created by Тимур Латуш on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct foodAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
